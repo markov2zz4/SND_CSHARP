2c9e5a76f1d370eeb521e7c2bd3a77665dc05400
MarkoW <albertnov@internet.ru>
2023-02-19 04:55:40 +0700

    Added project Кнопочка

---
+++ ButtonApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtonApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in MainGrid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button) el).Click += Button_Click;
+                    
+                }
+            }
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+
+            int r = rnd.Next(0, 256);
+            int g = rnd.Next(0, 256);
+            int b = rnd.Next(0, 256);
+
+            foreach (UIElement el in MainGrid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+
+                }
+            }
+
+            //btn_0.Background = new SolidColorBrush(Color.FromRgb((byte)r, (byte)g, (byte)b));
+        }
+    }
+}
