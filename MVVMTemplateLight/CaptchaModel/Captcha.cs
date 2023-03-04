f84ded643c38e12b1c6400ee74683554024c40a3
SHpetAI <shpetai@DOM201.LOCAL>
2023-03-04 16:07:52 +0700

    Added mvvm charp wpf

---
+++ MVVMTemplateLight/CaptchaModel/Captcha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CaptchaModel;
+
+public static class Captcha
+{
+    public static (string captchaHashCode, byte[] image) GenerateImageAsByteArray()
+    {
+        var code = new Random().Next(100_000, 999_999).ToString();
+        return (
+            GetHashString(code),
+            new CaptchaGenerator().GenerateImageAsByteArray(code.ToString()));
+    }
+}
